4abd67bfae1be6b39999d70a0012374670530748
SanketChidrewar <sanketchidrewar11@gmail.com>
2020-07-20 15:37:27 +0530

    added code for closure which is having function within function in self-invoking method

--- Javascript/Square.js
+++ Javascript/Square.js
@@ -1,29 +1,32 @@
 
 //named function
-
 function square(a){
 	console.log("Inside Named Function");
 	return a*a;
 }
-console.log(square(5));
+var num = prompt("ENTER A NUMBER TO GET SQUARE USING NAMED FUNCTION");
+alert("SQUARE IS :"+square(num));
+console.log(square(num));
 
 //Anonymous Function
-
 var sq = function(a){
 	console.log("Inside Anonymous Function");
 	return(a*a);
 }
-console.log(sq(5))
+var num = prompt("ENTER A NUMBER TO GET SQUARE USING Anonymous FUNCTION");
+alert("SQUARE IS :"+sq(num));
+console.log(sq(num));
 
 //Constructor Function
-
 var squ = new Function("a","console.log('Inside Constructor Function');return(a*a);")
-console.log(squ(5));
+var num = prompt("ENTER A NUMBER TO GET SQUARE USING Constructor FUNCTION");
+alert("SQUARE IS :"+squ(num));
+console.log(squ(num));
 
 //self-Invoking function
-
 var squa = (function(a){
 	console.log("Inside self-Invoking Function");
 	return a*a;
-}(5));
+}(prompt("ENTER A NUMBER TO GET SQUARE USING self-Invoking FUNCTION")));
+alert("SQUARE IS :"+squa);
 console.log(squa);
