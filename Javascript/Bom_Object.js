5e2159dd10b404bd5f9443c1a01d0b113a372c87
SanketChidrewar <sanketchidrewar11@gmail.com>
2020-07-22 16:55:10 +0530

    added screen object code for Browser Object Model

---
+++ Javascript/Bom_Object.js
@@ -0,0 +1,9 @@
+
+function GoToGoogle() 
+	{
+		var newWindow = window.open("http://www.google.com","msgWindow","width=1000,height=500");
+		// setTimeout(function () {
+		// 	alert("opened new window having Google home page and will closed in 5 sec"); 
+		// },5000);
+		// newWindow.close();
+	}
---
+++ Javascript/Browser_Object_Model/BOMScreenObject.js
@@ -0,0 +1,9 @@
+
+alert("\nScreen Height : "+screen.availHeight+
+	  "\nScreen Width : "+screen.availWidth+
+	  "\ncolor depth :"+screen.colorDepth+
+	  "\nwindow Height :"+window.innerHeight+
+	  "\nwindow Width :"+window.innerWidth);
+
+
+
