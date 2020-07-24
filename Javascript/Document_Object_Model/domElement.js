f3bde24f6d4157bd547552c611dd9db25797a74d
SanketChidrewar <sanketchidrewar11@gmail.com>
2020-07-24 17:00:53 +0530

    added code of DOM event handler for id class & tagName

---
+++ Javascript/Document_Object_Model/domElement.js
@@ -0,0 +1,9 @@
+
+
+		function execute() 
+		{
+		document.getElementById("greetings").innerHTML = "Welcome To Our Website";
+		// document.getElementsByClassName("hello").innerHTML = "Sanket here" ;
+		// document.getElementsByTagName('h4').innerHTML = "Pursuing PG-DAC from Sunbeam";		
+		}
+
