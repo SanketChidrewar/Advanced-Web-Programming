ea0cf5be1b2e82bc59b2037be2ffee7e795ab2f3
SanketChidrewar <sanketchidrewar11@gmail.com>
2020-07-25 00:52:18 +0530

    Assignment 1 on MY Friend Blog using HTML

--- Javascript/Document_Object_Model/domElement.js
+++ Javascript/Document_Object_Model/domElement.js
@@ -1,9 +1,17 @@
 
+		// window.onload = function(){
+		// 	execute();
+		// 	function execute(){
+		// document.getElementById("greetings").innerHTML = "Welcome To Our Website";
+		// document.getElementsByClassName("hello").innerHTML = "Sanket here" ;
+		// document.getElementsByTagName("h4").innerHTML = "Pursuing PG-DAC from Sunbeam";
+		// 	};
+		// }
 
 		function execute() 
 		{
 		document.getElementById("greetings").innerHTML = "Welcome To Our Website";
-		// document.getElementsByClassName("hello").innerHTML = "Sanket here" ;
-		// document.getElementsByTagName('h4').innerHTML = "Pursuing PG-DAC from Sunbeam";		
+		document.getElementsByClassName("hello").innerHTML = "Sanket here" ;
+		// document.getElementsByTagName("h4").innerHTML = "Pursuing PG-DAC from Sunbeam";		
 		}
 
